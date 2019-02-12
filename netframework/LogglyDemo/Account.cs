69f29590ee6156bc257b02f33c91d468e21524b6
Danny Arica <danny.arica@outlook.com>
2019-02-12 11:12:28 -0500

    Demo for using Loggly from a console App.

---
+++ netframework/LogglyDemo/Account.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogglyDemo
+{
+    public class Account
+    {
+        public int AccountId { get; set; }
+        public string Identifier { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+    }
+}
---
+++ netframework/LogglyDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using log4net;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogglyDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //log4net.Config.XmlConfigurator.Configure();
+            var account = new Account
+            {
+                AccountId = 2706,
+                Identifier = Guid.NewGuid().ToString(),
+                Name = "Luis Fonsi",
+                Phone = "+51944589632"
+            };
+
+
+            var logger = LogManager.GetLogger(typeof(Program));
+            
+            //Send plaintext
+            
+            logger.Info(JsonConvert.SerializeObject(account));
+
+            //Send an exception
+            //logger.Error("your log message", new Exception("your exception message"));
+
+            Console.ReadKey();
+        }
+    }
+}
